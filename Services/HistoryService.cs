4cec167fd4b4e5eadd388bdb2389aceed03ac251
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2022-11-11 23:59:37 +0100

    []

--- Services/HistoryService.cs
+++ Services/HistoryService.cs
@@ -6,22 +6,44 @@ namespace GrpcHistory.Services
     public class HistoryService: HistorySvc.HistorySvcBase
     {
 
+
         public override async Task HistoryWinStream(Empty _, IServerStreamWriter<HistoryWinEventResponseStream> responseStream, ServerCallContext context)
         {
 
-            var eventsWinList = new HistoryEvent[5].Select(h => {
-                var historyEvent = new HistoryEvent();
-                historyEvent.WinnerNickname = $"sylvain {h}";
-                historyEvent.ItemPrice = "10";
-                historyEvent.WinAt = DateTime.Now.ToFileTime();
-                historyEvent.ItemUrl = "http://localhost:3000";
-                return historyEvent;
-            }).ToArray();
+            // var eventsWinList = new HistoryEvent[5].Select(h => {
+            //     var historyEvent = new HistoryEvent();
+            //     historyEvent.WinnerNickname = $"sylvain {h}";
+            //     historyEvent.ItemPrice = "10";
+            //     historyEvent.WinAt = 1;
+            //     historyEvent.ItemUrl = "http://localhost:3000";
+            //     return historyEvent;
+            // }).ToArray();
 
-            return Task.FromResult(new HistoryWinEventResponseStream
-            {
-                historyEventList = eventsWinList
-            });
+            // Console.WriteLine(eventsWinList.Length);
+            // var rs = new Test();
+            // rs.Msg = "ok";
+            // await responseStream.WriteAsync(rs);
+            var data = new List<HistoryEvent> {
+                new HistoryEvent {
+                    WinnerNickname = "sylvain",
+                    ItemPrice = "10",
+                    WinAt = 1,
+                    ItemUrl = "http://localhost:3000"
+                },
+                new HistoryEvent {
+                    WinnerNickname = "sylvain2",
+                    ItemPrice = "10",
+                    WinAt = 1,
+                    ItemUrl = "http://localhost:3000"
+                }
+            };
+
+            var res = new HistoryWinEventResponseStream();
+            res.HistoryEventList.AddRange(data);
+
+            await responseStream.WriteAsync(res);
+
+            return;
             
         }
     }
